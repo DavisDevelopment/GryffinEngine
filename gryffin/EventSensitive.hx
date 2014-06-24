9caff0cf02070cea21ccd97b88ab7a8a5e14f033
Ryan Davis <epikit@gmail.com>
2014-06-24 01:18:23 -0500

    Reformat repo's directory structure

--- gryffin/EventSensitive.hx
+++
@@ -1,7 +0,0 @@
-package gryffin;
-
-interface EventSensitive {
-	function on( type:String, f:Dynamic -> Dynamic ):Void;
-	function unbind( type:String ):Void;
-	function emit( type:String, data:Dynamic ):Void;
-}
--- gryffin/display/Animation.hx
+++
@@ -1,6 +0,0 @@
-package gryffin.display;
-
-interface Animation {
-	function drawTo( surface:Surface, x:Int, y:Int, width:Int, height:Int ):Void;
-	function nextFrame():Void;
-}
